2238dc0c3165699857d21f09436f0551b4ee9e9f
Sparon <38852913+Sparoney@users.noreply.github.com>
2021-10-13 17:51:13 +1100

    channelID --> channel.id

--- discordTogether.js
+++ discordTogether.js
@@ -13,70 +13,70 @@ client.on('messageCreate', async message => {
     };
     if (message.content === prefix + "activity youtube") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'youtube').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
     };
     if (message.content === prefix + "activity youtubedev") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'youtubeDev').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtubeDev').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
     };
     if (message.content === prefix + "activity poker") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'poker').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'poker').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
     };
     if (message.content === prefix + "activity betrayal") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'betrayal').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'betrayal').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
     };
     if (message.content === prefix + "activity fishing") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'fishing').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'fishing').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
     };
     if (message.content === prefix + "activity chess") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'chess').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'chess').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
     };
     if (message.content === prefix + "activity chessdev") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'chessDev').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'chessDev').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
     };
     if (message.content === prefix + "activity lettertile") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'lettertile').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'lettertile').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
     };
     if (message.content === prefix + "activity wordsnack") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'wordsnack').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'wordsnack').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
     };
     if (message.content === prefix + "activity doodlecrew") {
         if (message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'doodlecrew').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'doodlecrew').then(async invite => {
                 return message.channel.send(`${invite.code}`);
             });
         };
